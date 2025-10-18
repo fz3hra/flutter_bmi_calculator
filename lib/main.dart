65d4121c4c2dabebf575a0f0672b5a49b745c4c2
fz3hra <mujorefaatimah@gmail.com>
2025-10-19 02:22:11 +0400

    ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-
 import 'package:flutter_bmi_calculator/calculator.dart';
+import 'package:flutter_bmi_calculator/home_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,91 +27,3 @@ class MyApp extends StatelessWidget {
 
 // TODO: BMI calculation
 //BMI = weight (kg) / height² (m²)
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late TextEditingController weightController;
-  late TextEditingController heightController;
-  String bmi = "";
-
-  @override
-  void initState() {
-    super.initState();
-    weightController = TextEditingController();
-    heightController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    weightController.dispose();
-    heightController.dispose();
-    super.dispose();
-  }
-
-  //   double? _parse(String text) {
-  //     if (text.trim().isEmpty) return null;
-  //     return double.tryParse(text.replaceAll(',', '.'));
-  //   }
-
-  //   double? _bmiCalc(double? weight, double? height) {
-  //     if (weight == null || height == null || height <= 0) return null;
-  //     return weight / pow(height, 2);
-  //   }
-
-  //   String displayBMI() {
-  //     final weight = _parse(weightController.text);
-  //     final height = _parse(heightController.text);
-
-  //     if (weight != null && height != null) {
-  //       var result = _bmiCalc(weight, height);
-  //       return result.toString();
-  //     }
-  //     return "";
-  //   }
-
-  @override
-  Widget build(BuildContext context) {
-    // var bmiResult = displayBMI();
-    final calc = const Calculator();
-    final bmiResult = calc.displayBMI(
-      weightController.text,
-      heightController.text,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calcualator'),
-      ),
-      body: Column(
-        children: [
-          Text("Weight (kg)"),
-          TextField(
-            key: Key("weight"),
-            controller: weightController,
-            onChanged: (_) => setState(() {}),
-          ),
-          Text("Height (m)"),
-          TextField(
-            key: Key("height"),
-            controller: heightController,
-            onChanged: (_) => setState(() {}),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text("BMI (kg/m^2)"),
-          Text(
-            key: Key("bmiResult"),
-            bmiResult,
-          ),
-        ],
-      ),
-    );
-  }
-}
